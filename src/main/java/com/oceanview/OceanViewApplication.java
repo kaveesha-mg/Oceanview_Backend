7e67cc29534f3c1e813b8cfdbdc1acf232d18184
Monali Kaveesha Gunasekara <kaveeperera942@gmail.com>
2026-02-28 17:44:51 +0400

    OceanViewApplication file

---
+++ src/main/java/com/oceanview/OceanViewApplication.java
@@ -0,0 +1,12 @@
+package com.oceanview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OceanViewApplication.class, args);
+    }
+}
